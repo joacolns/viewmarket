a0217c50171ab6297859f97265520fecb5a8b03c
njoaco <joaquinlucasnieto@gmail.com>
2025-02-08 00:09:06 -0300

    style: enhance ChartComponent layout with responsive design and improved aesthetics

--- app/components/ChartComponent/index.js
+++ app/components/ChartComponent/index.js
@@ -58,99 +58,102 @@ const ChartComponent = ({ crypto, chartData, rsiValue, macdValue }) => {
   const ema20 = calculateEMA(chartData, 20);
 
   return (
-    <Line
-      data={{
-        labels: chartData.map((_, index) => index),
-        datasets: [
-          {
-            label: `Price`,
-            data: chartData,
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            segment: {
-              borderColor: (ctx) => {
-                const { p0, p1 } = ctx;
-                return p1.raw > p0.raw ? 'green' : 'red';
-              }
+    <div className="max-w-full mx-auto p-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
+
+      <Line
+        data={{
+          labels: chartData.map((_, index) => index),
+          datasets: [
+            {
+              label: 'Price',
+              data: chartData,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              segment: {
+                borderColor: (ctx) => {
+                  const { p0, p1 } = ctx;
+                  return p1.raw > p0.raw ? 'green' : 'red';
+                }
+              },
+              tension: 0.1,
+              pointRadius: isMobile ? 0 : 3,
+              pointHoverRadius: isMobile ? 6 : 6
             },
-            tension: 0.1,
-            pointRadius: isMobile ? 0 : 3,
-            pointHoverRadius: isMobile ? 6 : 6
-          },
-          {
-            label: 'RSI',
-            data: Array(chartData.length).fill(rsiValue),
-            borderColor: 'rgb(255, 99, 132)',
-            fill: false,
-            borderWidth: 4,
-            tension: 0.1,
-            yAxisID: 'y1',
-            pointRadius: isMobile ? 0 : 0,
-            pointHoverRadius: isMobile ? 6 : 6
-          },
-          {
-            label: 'EMA 20',
-            data: ema20,
-            borderColor: 'rgb(255, 205, 86)',
-            borderWidth: 2,
-            tension: 0.1,
-            pointRadius: isMobile ? 0 : 1,
-            pointHoverRadius: isMobile ? 6 : 6
-          },
-          {
-            label: 'Bollinger Upper Band',
-            data: bollingerBands.upperBand,
-            borderColor: 'rgb(153, 102, 255)',
-            fill: '+1',
-            borderWidth: 2,
-            tension: 0.1,
-            pointRadius: isMobile ? 0 : 1,
-            pointHoverRadius: isMobile ? 6 : 6
-          },
-          {
-            label: 'Bollinger Lower Band',
-            data: bollingerBands.lowerBand,
-            borderColor: 'rgb(255, 159, 64)',
-            borderWidth: 2,
-            tension: 0.1,
-            pointRadius: isMobile ? 0 : 1,
-            pointHoverRadius: isMobile ? 6 : 6
-          }
-        ]
-      }}
-      options={{
-        responsive: true,
-        plugins: {
-          legend: { display: true },
-          tooltip: {
-            callbacks: {
-              label: function (tooltipItem) {
-                const price = Number(tooltipItem.raw);
-                return `Price: $${!isNaN(price) ? price.toFixed(2) : 'N/A'}`;
+            {
+              label: 'RSI',
+              data: Array(chartData.length).fill(rsiValue),
+              borderColor: 'rgb(255, 99, 132)',
+              fill: false,
+              borderWidth: 4,
+              tension: 0.1,
+              yAxisID: 'y1',
+              pointRadius: isMobile ? 0 : 0,
+              pointHoverRadius: isMobile ? 6 : 6
+            },
+            {
+              label: 'EMA 20',
+              data: ema20,
+              borderColor: 'rgb(255, 205, 86)',
+              borderWidth: 2,
+              tension: 0.1,
+              pointRadius: isMobile ? 0 : 1,
+              pointHoverRadius: isMobile ? 6 : 6
+            },
+            {
+              label: 'Bollinger Upper Band',
+              data: bollingerBands.upperBand,
+              borderColor: 'rgb(153, 102, 255)',
+              fill: '+1',
+              borderWidth: 2,
+              tension: 0.1,
+              pointRadius: isMobile ? 0 : 1,
+              pointHoverRadius: isMobile ? 6 : 6
+            },
+            {
+              label: 'Bollinger Lower Band',
+              data: bollingerBands.lowerBand,
+              borderColor: 'rgb(255, 159, 64)',
+              borderWidth: 2,
+              tension: 0.1,
+              pointRadius: isMobile ? 0 : 1,
+              pointHoverRadius: isMobile ? 6 : 6
+            }
+          ]
+        }}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: { display: true },
+            tooltip: {
+              callbacks: {
+                label: function (tooltipItem) {
+                  const price = Number(tooltipItem.raw);
+                  return `Price: $${!isNaN(price) ? price.toFixed(2) : 'N/A'}`;
+                }
               }
             }
-          }
-        },
-        scales: {
-          x: { display: false },
-          y: {
-            position: 'left',
-            beginAtZero: false,
           },
-          y1: {
-            position: 'right',
-            min: 0,
-            max: 100,
-            grid: { drawOnChartArea: false },
+          scales: {
+            x: { display: false },
+            y: {
+              position: 'left',
+              beginAtZero: false
+            },
+            y1: {
+              position: 'right',
+              min: 0,
+              max: 100,
+              grid: { drawOnChartArea: false }
+            }
+          },
+          elements: {
+            line: {
+              spanGaps: true
+            }
           }
-        },
-        elements: {
-          line: {
-            spanGaps: true,
-          }
-        }
-      }}
-    />
+        }}
+      />
+    </div>
   );
 };
 
